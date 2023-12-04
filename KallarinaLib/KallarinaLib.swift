2ad87e6901fe15649df186f30f0e9cffe9fbbe62
Pablo Sarango <pablo.sarango@ndeveloper.com>
2023-12-04 18:40:09 -0500

    Add funcs

---
+++ KallarinaLib/KallarinaLib.swift
@@ -0,0 +1,21 @@
+//
+//  KallarinaLib.swift
+//  KallarinaLib
+//
+//  Created by Pablo Sarango on 4/12/23.
+//
+
+import Foundation
+
+
+public final class KallarinaLib {
+    let name = "KallarinaLib";
+    
+    public func add(a: Int, b:Int) -> Int {
+        return a + b;
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b;
+    }
+}
